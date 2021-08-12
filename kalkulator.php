2c6ffdd2a2a364d485e82e61b0269d7135596c43
tria21 <sitiamaliafitriani@gmail.com>
2021-08-12 18:24:03 +0700

    https://github.com/tria21/dts_tugas8.git

---
+++ kalkulator.php
@@ -0,0 +1,26 @@
+<?php
+//function penjumlahan
+function jumlah($a, $b) {
+    return ($a + $b);
+}
+
+echo "Hasil Penjumlahan adalah ". jumlah(6,2) . "</br>";
+
+//function pengurangan
+function kurang($a, $b) {
+    return ($a - $b);
+}
+echo "Hasil Pengurangan adalah ". kurang(6,2) . "</br>";
+
+//function perkalian
+function kali($a, $b) {
+    return ($a * $b);
+}
+echo "Hasil Perkalian adalah ". kali(6,2) . "</br>";
+
+//function pembagian
+function bagi($a, $b){
+    return ($a / $b);
+}
+echo "Hasil Pembagian adalah ". bagi(6,2) . "</br>";
+?>
